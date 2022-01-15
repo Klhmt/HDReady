529ce257ea469714af1b01d39807e8b8e78e6436
Klhmt <clementjacquier77@gmail.com>
2022-01-15 14:29:42 +0100

    quality measures

--- hdr_v4_bis.py
+++ hdr_v4_bis.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from math import exp
 import timeit
+from statistics import stdev
 
 def openImages(path, imageNames):
     """This function open and store the images to merge
@@ -16,6 +17,20 @@ def openImages(path, imageNames):
         images.append(Image.open(path + name))
     return images
 
+def saturation_measure(r, g, b):
+    """This function measures saturation of a given pixel
+    which is the standard deviation within the R, G and B channels
+    """
+    channels = (r, g, b)
+    return stdev(channels)
+
+def exposition_measure(r, g, b, stdDeviation):
+    """This function measures the exposition of a given pixel
+    and apply a Gauss curve to it"""
+    channels = [r, g, b]
+    for x in channels:
+        x *= exp(-((x-127)**2)/(2*(stdDeviation**2)))
+    return r*g*b
 
 def generateNewImage(imagesToMerge: list, center:int, stdDeviation: int, coefficient:int = 1):
     """This function merge together each pixel of the different images
@@ -29,22 +44,18 @@ def generateNewImage(imagesToMerge: list, center:int, stdDeviation: int, coeffic
             for image in imagesToMerge:
                 pixel = image.getpixel((x, y))
                 brightness = 0.299 * pixel[0] + 0.587 * pixel[1] + 0.114 * pixel[2]     # Transformation of rbg to brightness value
-                if 10 <= brightness <= 180:
-                    bestPixel[0] += brightness * coefficient * exp(-((brightness-center)**2)/(2*stdDeviation**2))   # brightness value * coefficient
-                    bestPixel[1] += coefficient * exp(-((brightness-center)**2)/(2*stdDeviation**2))    # Coefficient
-            try:
-                bestPixel = round(bestPixel[0] / bestPixel[1])
-                delta = round(abs(brightness - bestPixel))
-            except ZeroDivisionError:
-                delta = 0
+                coeff = coefficient * exp(-((brightness-center)**2)/(2*stdDeviation**2))  # Coefficient
+                bestPixel[0] += brightness * coeff   # brightness value * coefficient
+                bestPixel[1] += coeff
+            deltas = [pixel[1]-pixel[0], pixel[2]-pixel[0], pixel[2]-pixel[1]]
+            bestPixel = round(bestPixel[0] / bestPixel[1])
+            r = round(bestPixel / (0.229 + 0.587 * (pixel[1]-pixel[0]) + 0.114 * (pixel[2]-pixel[0])))
+            v = round((bestPixel - 0.299*r) / (0.587 + 0.114 * (pixel[2]-pixel[1])))
+            b = round((bestPixel - (0.299*r + 0.587*v)) / 0.114)
+            finalImage.putpixel((x, y), (r, v, b))
+    finalImage.save(r"C:\Users\cjacq\Documents\Clément\Perso\Programmation\Photo_samples\hdr_new_deltas_100.png")
 
-            if brightness >= 127:
-                pixel = (pixel[0] - delta, pixel[1] - delta, pixel[2] - delta)
-            pixel = (pixel[0] + delta, pixel[1] + delta, pixel[2] + delta)
-            finalImage.putpixel((x, y), pixel)
-    finalImage.save(r"C:\Users\cjacq\Documents\Clément\Perso\Programmation\Photo_samples\hdr_v4bis_bornes_underexposed.png")
-
-
+print(saturation_measure(10, 14, 50))
 
 
 # Try
@@ -52,7 +63,7 @@ def generateNewImage(imagesToMerge: list, center:int, stdDeviation: int, coeffic
 #i = openImages('C:/Users/cjacq/Documents/Clément/Perso/Programmation/Photo_samples/', ('img2.jpg', 'img3.jpg', 'img4.jpg','img5.jpg', 'img6.jpg', 'img7.jpg', 'img1.jpg'))
 
 #i = openImages('C:/Users/cjacq/Documents/Clément/Perso/Programmation/Photo_samples/', ('1120716.jpg', '1120717.jpg', '1120718.jpg', '1120719.jpg', '1120720.jpg', '1120721.jpg', '1120722.jpg'))
-    i = openImages('C:/Users/cjacq/Documents/Clément/Perso/Programmation/Photo_samples/', ('P1130264.png', 'P1130265.png', 'P1130266.png', 'P1130267.png', 'P1130268.png', 'P1130269.png', 'P1130263.png'))
-generateNewImage(i, 127, 20)
+#i = openImages('C:/Users/cjacq/Documents/Clément/Perso/Programmation/Photo_samples/', ('P1130264.png', 'P1130265.png', 'P1130266.png', 'P1130267.png', 'P1130268.png', 'P1130269.png', 'P1130263.png'))
+#generateNewImage(i, 127, 100)
 
 
