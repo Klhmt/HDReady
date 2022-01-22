5b5cdf636f2cfe8f7496d9d273c173c2a20ac145
Klhmt <clementjacquier77@gmail.com>
2022-01-22 12:48:57 +0100

    wip

--- hdr_v4_bis.py
+++ hdr_v4_bis.py
@@ -2,6 +2,9 @@ from PIL import Image
 from math import exp
 import timeit
 from statistics import stdev
+import pickle
+
+coeff110 = pickle.load(open(r'C:\Users\cjacq\Documents\Clément\Perso\Programmation\HDReady\coeff110', 'rb'))
 
 def openImages(path, imageNames):
     """This function open and store the images to merge
@@ -17,7 +20,7 @@ def openImages(path, imageNames):
         images.append(Image.open(path + name))
     return images
 
-def saturation_measure(r, g, b):
+def saturation_measure(channels):
     """This function measures saturation of a given pixel
     which is the standard deviation within the R, G and B channels
     Args:
@@ -27,16 +30,15 @@ def saturation_measure(r, g, b):
     Return:
         int: standart deviation of r, g, b
     """
-    channels = (r, g, b)
     if stdev(channels) == 0:
         return 1
     return stdev(channels)
 
-def contrast_measure(r, g, b):
+def contrast_measure(channels):
     """WIP"""
     pass
 
-def exposition_measure(r, g, b, stdDeviation, multiplier: int=1):
+def exposition_measure(channels, stdDeviation, multiplier: int=1):
     """This function measures the exposition of a given pixel
     and apply a Gauss curve to it
     Args:
@@ -47,10 +49,10 @@ def exposition_measure(r, g, b, stdDeviation, multiplier: int=1):
         multiplier (int): multiplier
     Return:
         coeff (int): exposure weight of the pixel"""
-    channels = [r, g, b]
+    channels = list(channels)
     coeff = 1
     for x in range(3):
-        coeff *= multiplier * exp(-(((channels[x]-127)**2)/(2*(stdDeviation**2))))
+        coeff *= multiplier * coeff110[channels[x]]
     return coeff
 
 def gaussian(value, stdDeviation):
@@ -70,9 +72,9 @@ def generateNewImage(imagesToMerge: list):
             newPixel = [0, 0, 0]
             # Compute coefficients
             for image in imagesToMerge:
-                r, g, b = image.getpixel((x, y))
-                pixels.append((r, g, b))
-                coefficient.append(saturation_measure(r, g, b) * exposition_measure(r, g, b, 250, 1))
+                channels = image.getpixel((x, y))
+                pixels.append(channels)
+                coefficient.append(saturation_measure(channels) * exposition_measure(channels, 250, 1))
                 #coefficient.append(exposition_measure(r, g, b, 35))
             sum_coeff = sum(coefficient)
             # Normalization of coefficient values
@@ -83,22 +85,20 @@ def generateNewImage(imagesToMerge: list):
                     coefficient[z] = 0
             # Merging images => WIP 
             for i in range(len(coefficient)):
-                newPixel[0] += pixels[i][0] * coefficient[i]
-                newPixel[1] += pixels[i][1] * coefficient[i]
-                newPixel[2] += pixels[i][2] * coefficient[i]
+                for n in range(3):
+                    newPixel[n] += pixels[i][n] * coefficient[i]
             # Rounding values
             for a in range(3):
                 newPixel[a] = round(newPixel[a])
-            newPixel = tuple(newPixel)
             # Write new rgb values into the final image
-            finalImage.putpixel((x, y), newPixel)
-    finalImage.save(r"C:\Users\cjacq\Documents\Clément\Perso\Programmation\Photo_samples\hdr_paper_clavier_250_new_merge.png")
+            finalImage.putpixel((x, y), tuple(newPixel))
+    finalImage.save(r"C:\Users\cjacq\Documents\Clément\Perso\Programmation\Photo_samples\hdr_paper_110_new_merge_dico_list.jpg")
 
 
 # Try
-#i = openImages('C:/Users/cjacq/Documents/Clément/Perso/Programmation/Photo_samples/', ('img6.jpg', 'img5.jpg','img3.jpg'))
+i = openImages('C:/Users/cjacq/Documents/Clément/Perso/Programmation/Photo_samples/', ('img6.jpg', 'img5.jpg','img3.jpg'))
 #i = openImages('C:/Users/cjacq/Documents/Clément/Perso/Programmation/Photo_samples/', ('img2.jpg', 'img3.jpg', 'img4.jpg','img5.jpg', 'img6.jpg', 'img7.jpg', 'img1.jpg'))
-i = openImages('C:/Users/cjacq/Documents/Clément/Perso/Programmation/Photo_samples/', ('clavier1_comp.jpg', 'clavier2_comp.jpg'))
+#i = openImages('C:/Users/cjacq/Documents/Clément/Perso/Programmation/Photo_samples/', ('clavier1_comp.jpg', 'clavier2_comp.jpg'))
 #i = openImages('C:/Users/cjacq/Documents/Clément/Perso/Programmation/Photo_samples/', ('1120716.jpg', '1120717.jpg', '1120718.jpg', '1120719.jpg', '1120720.jpg', '1120721.jpg', '1120722.jpg'))
 #i = openImages('C:/Users/cjacq/Documents/Clément/Perso/Programmation/Photo_samples/', ('clavier1_comp.jpg', 'clavier2_comp.jpg'))
 generateNewImage(i)
