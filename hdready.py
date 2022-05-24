ca146064fcf3d7730bab0689c480407475749b8f
Klhmt <clementjacquier77@gmail.com>
2022-05-24 19:51:29 +0200

    docstrings

--- hdready.py
+++ hdready.py
@@ -8,40 +8,67 @@ from os.path import isfile, join, dirname
 from multiprocessing import Pool
 
 class Images_stack():
-    """WIP"""
+    """A class to represent a stack of images to merge"""
     
-    def __init__(self, img_list, stdDeviation, processes):
+    def __init__(self, img_list:list, stdDeviation:int, processes:int):
+        """
+        Constructs all the necessary attributes for the stack object
+        
+        Args:
+            img_list (list): list containing PIL Image objects
+            stdDeviation (int): parameter of the Gaussian functions directly 
+                                impacting the final dynamic range
+            processes (int): number of processes used for multiprocessing
+        
+        """
         self.stack = img_list
         self.stdDeviation = stdDeviation
         self.height = self.stack[0].height
         self.width = self.stack[0].width
         self.processes = processes
-        self.coefficients = {value: round((exp( - ((value - 127) ** 2) / (2 * self.stdDeviation ** 2))), 6) 
-            for value in range(0, 256)}
-        self.parts = [(x * floor(self.width / self.processes), 0, (x + 1) * floor(self.width / self.processes), self.height) for x in range(self.processes - 1)] + [((self.processes - 1) * floor(self.width / self.processes), 0, self.width, self.height)]
+        self.parts = [(x * floor(self.width / self.processes), 0,
+                      (x + 1) * floor(self.width / self.processes), self.height)
+                      for x in range(self.processes - 1)] 
+        self.parts +=  [((self.processes - 1) * 
+                        floor(self.width / self.processes), 
+                        0, self.width, self.height)]
 
 
     def images_crop(self):
-        """This function cuts the images stack into processes ones 
-        CAS OU PROCESSE = 1 cf hdr ready
-
-        Args:
-            processes (int): number of cores used by the fusion process and number
-                of parts of the images.
+        """
+        This function cuts the images stack into processes ones 
 
         Return:
-            (list): list containing Images_stack objects 
+            (list): list containing Divided_images_stack objects 
 
         """
         if self.processes == 1:
             return self
-        return [Divided_images_stack([img.crop(self.parts[x]) for img in self.stack], self.stdDeviation, self.parts, self.width, self.height) for x in range(self.processes)]
+        return [Divided_images_stack([img.crop(self.parts[x]) 
+                                      for img in self.stack], 
+                                     self.stdDeviation, self.parts, self.width, 
+                                     self.height) for x in range(self.processes)]
 
 
 class Divided_images_stack():
-    """WIP"""
+    """A class of pieces of images stack"""
 
-    def __init__(self, img_list, stdDeviation, parts, final_width, final_height):
+    def __init__(self, img_list:list, stdDeviation:int, parts:list, 
+                 final_width:int, final_height:int):
+        """
+        Constructs all the necessary attributes for the divided images 
+        stack object
+        
+        Args:
+            img_list (list): list containing PIL Image objects
+            stdDeviation (int): parameter of the Gaussian functions directly 
+                                impacting the final dynamic range
+            parts (list): list containing tuples of coordinates defining 
+                          a piece of a divided image
+            final_width (int): width of the original and final images
+            final_height (int): height of the original and final images
+        
+        """
         self.stack = img_list
         self.stdDeviation = stdDeviation
         self.parts = parts
@@ -49,13 +76,15 @@ class Divided_images_stack():
         self.height = self.stack[0].height
         self.final_width = final_width
         self.final_height = final_height
-        self.coefficients = {value: round((exp( - ((value - 127) ** 2) / (2 * self.stdDeviation ** 2))), 6) 
+        self.coefficients = {value: round((exp( - ((value - 127) ** 2) / 
+                                               (2 * self.stdDeviation ** 2))), 6) 
             for value in range(0, 256)}
     
     
-    def exposition_measure(self, channels:tuple)->'weight':
-        """This function measures the exposition of a given pixel
-        and apply a Gauss curve to compute the weight
+    def exposition_measure(self, channels:tuple):
+        """
+        This function measures the exposition of a given pixel
+        and apply a Gauss curve to compute and return the weight
 
         Arg:
             channels (tuple): tuple that contains red, green and blue channel
@@ -70,17 +99,16 @@ class Divided_images_stack():
         return weight
 
 
-    def generate_new_image(self)->'hdr piece of image':
-        """This function merges the images into a new one
-
-        Arg:
-            imagesToMerge (list): list containing the input images
+    def generate_new_image(self):
+        """
+        This function merges the images into a new one
 
         Return:
-            finalImage (image object): a part of the final image
+            finalImage (PIL Image object): a part of the final image
 
         """
-        finalImage = Image.new(mode="RGB", size=(self.final_width, self.final_height))
+        finalImage = Image.new(mode="RGB", size=(
+            self.final_width, self.final_height))
 
         for y in range(self.height):
             for x in range(self.width):
@@ -104,20 +132,22 @@ class Divided_images_stack():
         return finalImage
 
 
-    def reassemble(self)->'final image':
-        """This function reassembles the parts of the final image
+    def reassemble(self):
+        """
+        This function reassembles the parts of the final image
         
         Return:
             (Image object): final image
 
         """
-        finalImage = Image.new(mode="RGB", size=(self.final_width, self.final_height))
+        finalImage = Image.new(mode="RGB", 
+                               size=(self.final_width, self.final_height))
         for x in range(len(self.stack)):
             finalImage.paste(self.stack[x], self.parts[x][:2])
         return finalImage
     
 
-def open_images(imagesFolderPath:str)->'list containing images':
+def open_images(imagesFolderPath:str):
     """This function open and store the images to merge into a list
     
     Args:
@@ -134,12 +164,24 @@ def open_images(imagesFolderPath:str)->'list containing images':
     return images
 
 
-def hdr(imagesToMerge):
+def hdr(imagesToMerge:Divided_images_stack):
+    """Function called by pool"""
     return imagesToMerge.generate_new_image()
 
 
-def start(imgFolderPath, finalPath, stdDeviation, processes):
-    """main function that launche the HDR process"""
+def start(imgFolderPath:str, finalPath:str, stdDeviation:int, processes:int):
+    """
+    Main function that launches the HDR process
+    
+    Args:
+        imgFolderPath (str): path to the folder containing the images to merge
+        finalPath (str): path to the final image file 
+                         (like "C:/user/my_hdry_photo/hdr_edited.jpg")
+        stdDeviation (int): parameter of the Gaussian functions directly 
+                            impacting the final dynamic range
+        processes (int): number of processes used for multiprocessing
+    
+    """
     if __name__ == '__main__':
         # Check if the folder containing the images exists or not
         if os.path.isdir(imgFolderPath):
@@ -159,7 +201,9 @@ def start(imgFolderPath, finalPath, stdDeviation, processes):
         divided_stack = stack.images_crop()
         # HDR merging
         with Pool(processes) as p:
-            results = Divided_images_stack(p.map(hdr, divided_stack), stack.stdDeviation, stack.parts, stack.width, stack.height)
+            results = Divided_images_stack(p.map(hdr, divided_stack), 
+                                           stack.stdDeviation, stack.parts, 
+                                           stack.width, stack.height)
         # Reassembling the pieces of the final image
         final_image = results.reassemble()
         final_image.save(finalPath)  
